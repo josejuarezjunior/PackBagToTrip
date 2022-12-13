833c8a00461b3e38f98d2ad62614d1dc8b190a90
Junior <Josejuarez.Junior@smartm.com>
2022-12-12 21:49:17 -0300

    Creating rule to update item

--- js/main.js
+++ js/main.js
@@ -13,15 +13,24 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find( elemento => elemento.nome === nome.value);
+
     // Criando um objeto
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     };
 
-    criarElemento(itemAtual);
-
-    itens.push(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizarElemento(itemAtual);
+        itens[existe.id] = itemAtual;
+    } else {
+        itemAtual.id = itens.length;
+        criarElemento(itemAtual);
+        itens.push(itemAtual);
+    }
+    
     // O localStorage só lê um elemento do tipo JSON, uma string.
     // portanto ao enviar o objeto, é preciso utilizar "JSON.stringify(objeto)"
     // para transformá-lo em texto.
@@ -40,10 +49,16 @@ function criarElemento(item) {
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
-
+    // Criando o Data attribute
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
     
+}
+
+function atualizarElemento(item) {
+    // Capturando o item através do Data Attribute
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
